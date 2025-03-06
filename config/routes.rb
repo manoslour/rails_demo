3a0ef125a354cd49b1fd635120335873f7592fdf
Kostas Familonidis <kostas.familonidis@gmail.com>
2025-03-06 13:03:04 +0200

    custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+
   root "posts#index"
 end
