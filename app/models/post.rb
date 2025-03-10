9bba975633b6918de1b72605906e80f34d741ac0
Tasos Maschalidis <info@tashows.com>
2025-03-10 13:05:04 +0200

    Add scopes and enum to post. Update comments association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,16 @@
 class Post < ApplicationRecord
-
   validates :content, presence: true
 
   belongs_to :user
 
-  has_many :comments
+  has_many :comments, as: :commentable
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
+
+  scope :published, -> { where(published_at: ..Time.zone.now) }
+  scope :published_before, -> (time) { where(published_at: ..time) }
+  scope :published_after, -> (time) { where(published_at: time..) }
+  scope :empty, -> { where(content: ["", nil]) } # content is null OR content = ""
+
+  enum :status, %i[draft visible archived], prefix: true # %i defines options as symbols
 end
