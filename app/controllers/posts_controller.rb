9587bd7a2c1c364078465adccfb45cba49ec5cb1
Tasos Maschalidis <info@tashows.com>
2025-03-11 16:55:27 +0200

    Add includes and solve N+1 query

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :check_owner, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.published
+    @posts = Post.includes(:user, :sections).published
   end
 
   def new
