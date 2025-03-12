0f7e112ed0d843af8e82ab2c081bdebde4b0314d
Kostas Familonidis <kostas.familonidis@gmail.com>
2025-03-12 12:01:52 +0200

    add user tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :user do
-    
+    username { 'Test' }
+    email { 'test@demo.com' }
+    password { '12345678' }
+
+    trait :valid do
+    end
+
+    trait :invalid do
+      password { '' }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { subject.save }
+
+  context 'when valid' do
+    subject { build(:user) }
+
+    it 'persists' do
+      expect(subject).to be_persisted
+    end
+
+    it 'does not have errors' do
+      expect(subject.errors).to be_blank
+    end
+  end
+
+  context 'when invalid' do
+    subject { build(:user, :invalid) }
+
+    it 'does not persist' do
+      expect(subject).not_to be_persisted
+    end
+
+    it 'does have errors' do
+      expect(subject.errors).not_to be_blank
+    end
+  end
 end
