555bb1a0833b4cc8ecb98395445eb374ea983fb3
Tasos Maschalidis <info@tashows.com>
2025-03-13 16:58:44 +0200

    Broadcast post prepend via model callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+
+  after_create :broadcast_comment
+
+  private
+
+  def broadcast_comment
+    self.broadcast_prepend_to [self.commentable, :comments], target: 'comments', partial: "comments/comment", locals: { comment: self }
+  end
 end
